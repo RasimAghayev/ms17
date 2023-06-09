56dc01dd25198ce4e7927979ee8c4de480a44259
agilisayev <isayevagil@gmail.com>
2023-06-09 20:53:37 +0400

    added log

--- src/main/java/com/example/ms17/Ms17Application.java
+++ src/main/java/com/example/ms17/Ms17Application.java
@@ -1,13 +1,18 @@
 package com.example.ms17;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class Ms17Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ms17Application.class, args);
+
+
+		log.info("Application is starting!");
 	}
 
 }
