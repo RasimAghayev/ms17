5a5ba0cf208ebe0e5acadaa1093c2e780fafaf07
Rasim Aghayev <rasimaqayev@gmail.com>
2023-07-14 02:56:36 +0400

    Unit Test

---
+++ src/main/java/com/example/ms17/service/Calculator.java
@@ -0,0 +1,9 @@
+package com.example.ms17.service;
+
+public interface Calculator {
+    double sum(double a, double b);
+    double sub(double a, double b);
+    double divide(double a, double b);
+    double multiple(double a, double b);
+
+}
---
+++ src/main/java/com/example/ms17/service/impl/CalculatorImpl.java
@@ -0,0 +1,28 @@
+package com.example.ms17.service.impl;
+
+import com.example.ms17.service.Calculator;
+
+public class CalculatorImpl implements Calculator {
+    @Override
+    public double sum(double a, double b) {
+        return a+b;
+    }
+
+    @Override
+    public double sub(double a, double b) {
+        return a-b;
+    }
+
+    @Override
+    public double divide(double a, double b) {
+        if (b!=0){
+            return a/b;
+        }
+        throw new ArithmeticException("b can not be 0");
+    }
+
+    @Override
+    public double multiple(double a, double b) {
+        return a*b;
+    }
+}
